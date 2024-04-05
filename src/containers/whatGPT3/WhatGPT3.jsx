28f43432ea063499278ce52fad663b91cbccd94f
chevonnelise <elise.chevonne@gmail.com>
2024-04-05 17:24:48 +0000

    edit feature and whatgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Feature from '../../components/feature/Feature';
 import './whatGPT3.css';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-features">
+        <Feature title="What is GPT-3" text="ChatGPT-3, short for 'Chat Generative Pre-trained Transformer 3,' is an advanced natural language processing model developed by OpenAI. It is the third iteration of the GPT (Generative Pre-trained Transformer) series, following GPT and GPT-2. GPT-3 is trained on a massive dataset of diverse text from the internet, allowing it to generate human-like text and respond to prompts in a wide range of topics and styles."/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">Create possibilities beyond your imagination.</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt__whatgpt3-container">
+        <Feature title="Chatbots" text="ChatGPT-3: Revolutionizing conversational AI with human-like text generation."/>
+        <Feature title="Knowledgebase" text="ChatGPT-3: Advanced AI generates human-like text, revolutionizing conversational understanding."/>
+        <Feature title="Education" text="GPT-3: Text gen, translation, summarization, QA, integrated for diverse linguistic needs."/>
+      </div>
+    </div>
   )
 }
 
